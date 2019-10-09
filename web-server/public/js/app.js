268f6c18615fe3bc83590254a530e47f88baf54b
vincenicoara <vincenicoara@gmail.com>
2019-10-09 11:13:28 -0700

    Configured code for heroku deployment

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     m1.textContent = "Loading..." 
     m2.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 
         response.json().then((data) => {
             if (data.error){
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 console.log(__dirname)
 console.log(__filename)
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
 })
